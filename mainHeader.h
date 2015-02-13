a6d6ef49226dbbc510cf2916eebf39f7556574a2
Galax27 <costin_tapean@yahoo.com>
2015-02-13 12:07:54 +0200

    Create mainHeader.h

---
+++ mainHeader.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <GL\glew.h>
+#include <GL\glut.h>
+
+
+//Object
+#include "headerFiles\objectClass\Point.h"
+#include "headerFiles\objectClass\Object.h"
+
+//Player
+#include "headerFiles\objectClass\Player.h"
+#include "headerFiles\objectClass\Surface.h"
+
+//Input
+#include "headerFiles\inputClass\Keyboard.h"
+
+//Graphic
+#include "headerFiles\graphicClass\Camera.h"
+#include "headerFiles\graphicClass\Interface.h"
+
+//Engine
+#include "headerFiles\engineClass\Engine.h"
